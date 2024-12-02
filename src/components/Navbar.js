4de773a137fedfb0d2bc6d571218f9a2ba5278b7
Vomelhor <vitorpnn2009@hotmail.com>
2024-12-02 17:57:34 -0300

    Initial commit with React setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#inicio">Início</a></li>
+        <li><a href="#produtos">Produtos</a></li>
+        <li><a href="#quem-somos">Quem Somos</a></li>
+        <li><a href="#contato">Contato</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
